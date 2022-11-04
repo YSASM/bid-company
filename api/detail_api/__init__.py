da939b314362802f5a6ad30bac50472bea1e2665
Wujiefeng <1613921123@qq.com>
2022-11-04 14:18:44 +0800

    update

--- api/detail_api/__init__.py
+++ api/detail_api/__init__.py
@@ -1,10 +1,10 @@
 
 def get_list():
     list_ = []
-    from api.detail_api.api_yuanlue_detail import YuanlueDetail
-    # list_.append(['yuanlue',YuanlueDetail])#yuanlue开关
     from api.detail_api.api_kanzhunwang_detail import KanzhunwangDetail
     list_.append(['kanzhunwang',KanzhunwangDetail])
     from api.detail_api.api_qichacha_detail import QichachaDetail
     list_.append(['qichacha',QichachaDetail])
+    from api.detail_api.api_yuanlue_detail import YuanlueDetail
+    list_.append(['yuanlue',YuanlueDetail])#yuanlue开关
     return list_
