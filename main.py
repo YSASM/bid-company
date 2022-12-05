d4b33aa75b6650d37bdfe42b455723041cc6fb5a
YSASM <1613921123@qq.com>
2022-12-05 20:56:53 +0800

    update

--- main.py
+++ main.py
@@ -164,6 +164,11 @@ class Main(object):
     @api.route('/details',methods=['get'])
     def details():
         start = int(float(time())*1000)
+        if page:
+            page = int(page)
+        limit = request.args.get('limit')
+        if limit:
+            limit = int(limit)
         try:
             words = request.args.get('words')
             page = request.args.get('page')
