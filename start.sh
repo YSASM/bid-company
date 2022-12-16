78e17126c7654208487bcedd5f96998479c03a74
YSASM <1613921123@qq.com>
2022-12-16 21:53:44 +0800

    123

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -x
+ROOT=$(cd `dirname $0`; pwd)
+cd $ROOT
 echo "[program:FlaskGunicornSupervisor]" >> /etc/supervisord.conf
 if [ "$MODE"x == "test"x ];then
     echo "command=nohup /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
@@ -12,9 +15,6 @@ echo "autorestart=true ;" >> /etc/supervisord.conf
 echo "startretires=5   ;" >> /etc/supervisord.conf
 supervisord -c /etc/supervisord.conf 
 supervisorctl update
-set -x
-ROOT=$(cd `dirname $0`; pwd)
-cd $ROOT
 
 if [ "$MODE"x == "test"x ];then
     while [ true ];do
