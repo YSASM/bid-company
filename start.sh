a77a0ee256cbbd1e8536cfd6a87a3e2cf2614971
YSASM <1613921123@qq.com>
2022-12-16 21:59:17 +0800

    123

--- start.sh
+++ start.sh
@@ -2,17 +2,17 @@
 set -x
 ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
-echo "[program:FlaskGunicornSupervisor]" >> /etc/supervisord.conf
-if [ "$MODE"x == "test"x ];then
-    echo "command=nohup /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
-else
-    echo "command=exec /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
-fi
-echo "directory=/app  ;" >> /etc/supervisord.conf
-echo "user=root      ;" >> /etc/supervisord.conf
-echo "autostart=true   ;" >> /etc/supervisord.conf
-echo "autorestart=true ;" >> /etc/supervisord.conf
-echo "startretires=5   ;" >> /etc/supervisord.conf
+# echo "[program:FlaskGunicornSupervisor]" >> /etc/supervisord.conf
+# if [ "$MODE"x == "test"x ];then
+#     echo "command=nohup /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
+# else
+#     echo "command=exec /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
+# fi
+# echo "directory=/app  ;" >> /etc/supervisord.conf
+# echo "user=root      ;" >> /etc/supervisord.conf
+# echo "autostart=true   ;" >> /etc/supervisord.conf
+# echo "autorestart=true ;" >> /etc/supervisord.conf
+# echo "startretires=5   ;" >> /etc/supervisord.conf
 # supervisord -c /etc/supervisord.conf 
 # supervisorctl update
 
