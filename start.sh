04d5febd163af64eef5b7daf17cc41e276b8b200
YSASM <1613921123@qq.com>
2022-12-16 22:11:20 +0800

    123

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
 # /usr/bin/python3 /app/run.py
 echo "[program:FlaskGunicornSupervisor]" >> /etc/supervisord.conf
-echo "command=exec /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
+echo "command=exec /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> /app/log/run.log 2>&1" >> /etc/supervisord.conf
 echo "directory=/app  ;" >> /etc/supervisord.conf
 echo "user=root      ;" >> /etc/supervisord.conf
 echo "autostart=true   ;" >> /etc/supervisord.conf
