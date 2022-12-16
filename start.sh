d7b7266f7cd509bc71ec84adeba0e8451b2bb68b
YSASM <1613921123@qq.com>
2022-12-16 21:51:51 +0800

    123

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "[program:FlaskGunicornSupervisor]" >> /etc/supervisord.conf
-echo "command=/usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api" >> /etc/supervisord.conf
+if [ "$MODE"x == "test"x ];then
+    echo "command=nohup /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
+else
+    echo "command=exec /usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api  >> log/run.log 2>&1" >> /etc/supervisord.conf
+fi
 echo "directory=/app  ;" >> /etc/supervisord.conf
 echo "user=root      ;" >> /etc/supervisord.conf
 echo "autostart=true   ;" >> /etc/supervisord.conf
@@ -8,16 +12,12 @@ echo "autorestart=true ;" >> /etc/supervisord.conf
 echo "startretires=5   ;" >> /etc/supervisord.conf
 supervisord -c /etc/supervisord.conf 
 supervisorctl update
-supervisorctl stop FlaskGunicornSupervisor
 set -x
 ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
 
 if [ "$MODE"x == "test"x ];then
     while [ true ];do
-        nohup supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
+        supervisorctl restart FlaskGunicornSupervisor
         sleep 86400000
     done
-else
-    exec supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
-fi
