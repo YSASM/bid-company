c1ca2475c7dd1b953948d86a0668ebbe59102838
YSASM <1613921123@qq.com>
2022-12-16 21:06:23 +0800

    123

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ set -x
 ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
 
-if [ "$MODE"x == "test"x ];then
-    while [ true ];do
-        nohup supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
-        sleep 86400000
-    done
-else
-    exec supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
-fi
+# if [ "$MODE"x == "test"x ];then
+#     while [ true ];do
+#         nohup supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
+#         sleep 86400000
+#     done
+# else
+#     exec supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
+# fi
