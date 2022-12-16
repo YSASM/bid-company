3d81d4a6209cf34c54b894eae6490a4a1bded2a8
YSASM <1613921123@qq.com>
2022-12-16 21:28:34 +0800

    123

--- start.sh
+++ start.sh
@@ -1,15 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
+cp /usr/local/python-3.9/bin/gunicorn /usr/bin/
+echo "[program:FlaskGunicornSupervisor]" >> /etc/supervisord.conf
+echo "command=/usr/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api" >> /etc/supervisord.conf
+echo "directory=/app  ;" >> /etc/supervisord.conf
+echo "user=root      ;" >> /etc/supervisord.conf
+echo "autostart=true   ;" >> /etc/supervisord.conf
+echo "autorestart=true ;" >> /etc/supervisord.conf
+echo "startretires=5   ;" >> /etc/supervisord.conf
+supervisord -c /etc/supervisord.conf 
+supervisorctl update
+supervisorctl stop FlaskGunicornSupervisor
 set -x
 ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
 
 if [ "$MODE"x == "test"x ];then
     while [ true ];do
-        supervisorctl stop FlaskGunicornSupervisor
         nohup supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
         sleep 86400000
     done
 else
-    supervisorctl stop FlaskGunicornSupervisor
     exec supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
 fi
