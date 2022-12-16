b64803766e8334393e31126004bcc9adc626dbde
YSASM <1613921123@qq.com>
2022-12-16 20:33:06 +0800

    update

--- start.sh
+++ start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 cp /usr/local/python-3.9/bin/gunicorn /usr/bin/
 yum install supervisor
-sudo touch /usr/etc/supervisor/conf.d/config.conf
-sudo gedit /usr/etc/supervisor/conf.d/config.conf
-echo "[program:FlaskGunicornSupervisor]\n
-command=/usr/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api\n
-directory=/app  ; \n
-user=root      ; \n
-autostart=true   ; \n
-autorestart=true ; \n
-startretires=5   ;" > /usr/etc/supervisor/conf.d/config.conf
+echo "[program:FlaskGunicornSupervisor]"
+"command=/usr/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api"
+"directory=/app  ;"
+"user=root      ;"
+"autostart=true   ;"
+"autorestart=true ;"
+"startretires=5   ;" >> /etc/supervisord.conf
 supervisorctl update
 set -x
 ROOT=$(cd `dirname $0`; pwd)
