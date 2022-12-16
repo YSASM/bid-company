65cbdf67a599343aed64a772653af44108b620f1
YSASM <1613921123@qq.com>
2022-12-16 21:11:20 +0800

    update

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
-supervisorctl stop FlaskGunicornSupervisor
-supervisord -c /etc/supervisord.conf 
+#!/bin/bash 
 set -x
 ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
 
-# if [ "$MODE"x == "test"x ];then
-#     while [ true ];do
-#         nohup supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
-#         sleep 86400000
-#     done
-# else
-#     exec supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
-# fi
+if [ "$MODE"x == "test"x ];then
+    while [ true ];do
+        supervisorctl stop FlaskGunicornSupervisor
+        nohup supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
+        sleep 86400000
+    done
+else
+    supervisorctl stop FlaskGunicornSupervisor
+    exec supervisorctl start FlaskGunicornSupervisor >> log/run.log 2>&1
+fi
