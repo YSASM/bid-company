a172620f61d3dcb0d401c74454133c1d17c7276c
Wujiefeng <1613921123@qq.com>
2022-12-20 13:29:20 +0800

    update

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@ set -x
 ROOT=$(cd `dirname $0`; pwd)
 cd $ROOT
 mv /app/status.sh /usr/bin/api_status
+# 查询服务状态
 mv /app/api_restart.sh /usr/bin/api_restart
+# 重启api数据接口服务
 mv /app/api_getid_restart.sh /usr/bin/api_getid_restart
-# /usr/bin/python3 /app/run.py
+# 重启api查询id服务
 echo "[program:Api]" >> /etc/supervisord.conf
 echo "command=/usr/local/python-3.9/bin/gunicorn -w 5 -b 0.0.0.0:9252 run:api" >> /etc/supervisord.conf
 echo "directory=/app  ;" >> /etc/supervisord.conf
@@ -25,9 +27,9 @@ echo "stdout_logfile = /app/log/api_getid_run.log" >> /etc/supervisord.conf
 supervisord -c /etc/supervisord.conf 
 supervisorctl update
 supervisorctl start Api
-# 启动查询api服务
+# 启动api数据接口服务
 supervisorctl start Api_getid
-# 启动获取idapi服务
+# 启动api查询id服务
 if [ "$MODE"x == "test"x ];then
     while [ true ];do
         nohup /usr/bin/python3 main.py >> log/run.log 2>&1
